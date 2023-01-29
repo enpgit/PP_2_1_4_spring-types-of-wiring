12c04f5d25343dcda2908c55ba5280a46593d007
Evgenij Pashkin <e.n.pashkin@gmail.com>
2023-01-29 16:24:45 +0300

    All rules

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    private Duck5 isduck;
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + isduck.toString();
+    }
+
+    @Autowired
+    public void setIsduck(Duck5 isduck) {
+        this.isduck = isduck;
     }
 }
